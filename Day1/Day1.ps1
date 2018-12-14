1e2c04af713a32c618f020a8c14aad7b9aa10c1a
Aparna138 <45551191+Aparna138@users.noreply.github.com>
2018-12-14 11:45:17 +0530

    Update Day1.ps1

--- Day1/Day1.ps1
+++ Day1/Day1.ps1
@@ -2,19 +2,22 @@
 [INT]$StartNumber = Read-Host -Prompt "Enter StartNumber"
 [INT]$EndNumber= Read-Host -Prompt "Enter EndNumber"
 [INT]$NoofLines=Read-Host -Prompt "Enter NoOflines"
+[INT]$var1=Read-Host -Prompt "div1"
+[INT]$var2=Read-Host -Prompt "div2"
+[INT]$var3=Read-Host -Prompt "div3"
 [INT]$Original=$StartNumber
 $output=""
 for ($StartNumber; $StartNumber -le $EndNumber; $StartNumber++)
 {   
-  if ( $StartNumber%15 -eq 0 ) 
+  if ( $StartNumber%var1 -eq 0 ) 
         { 
             $output="FiZZBUZZ"
         }
-   elseif ($StartNumber%5 -eq 0 )
+   elseif ($StartNumber%var2 -eq 0 )
         {
             $output="BUZZ"
         }
-   elseif($StartNumber%3 -eq 0)
+   elseif($StartNumber%var3 -eq 0)
        {
           $output="fizz"
        }
